ba138f25a715d8e7acc3324222f91daa2aecc87c
AzharTurk <azhar9380khan@gmail.com>
2022-08-19 15:25:44 +0530

    2_translate_rtl_to_lrt_language

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from "@ngx-translate/core";
+import { DOCUMENT } from "@angular/common";
+import { Inject } from "@angular/core";
 
 @Component({selector: 'app-header',templateUrl: './header.component.html',styleUrls:['./header.component.scss']})
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(
+    private translateService: TranslateService,
+    @Inject(DOCUMENT) private document: Document
+  ) {
+    this.translateService.setDefaultLang('en');
+    this.translateService.addLangs(['en', 'ar']);
+  }
+ 
   ngOnInit(): void {
+
+  }
+  changeLangage(lang: string) {
+    let htmlTag = this.document.getElementsByTagName(
+      "html"
+    )[0] as HTMLHtmlElement;
+    htmlTag.dir = lang === "ar" ? "rtl" : "ltr";
+    this.translateService.setDefaultLang(lang);
+    this.translateService.use(lang);
+    this.changeCssFile(lang);
   }
 
+  changeCssFile(lang: string) {
+    
+    let headTag = this.document.getElementsByTagName(
+      "head"
+    )[0] as HTMLHeadElement;
+    let existingLink = this.document.getElementById(
+      "langCss"
+    ) as HTMLLinkElement;
+
+    let bundleName = (lang === "ar" ? "src/app/themes/ar_css/AdminLTE.min.css" : "src/app/themes/en_css/AdminLTE.min.css");
+
+    if (existingLink) {
+      existingLink.href = bundleName;
+    } else {
+      let newLink = this.document.createElement("link");
+      newLink.rel = "stylesheet";
+      newLink.type = "text/css";
+      newLink.id = "langCss";
+      newLink.href = bundleName;
+      headTag.appendChild(newLink);
+    }
+  }
 }
